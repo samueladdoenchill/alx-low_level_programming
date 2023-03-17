96e9c8f9e201c99d2bc32099218083c7f786d5b7
samueladdoenchill <samueladdoenchill@gmail.com>
2023-03-16 18:22:50 -0700

    add files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, strlen(str));
+
+	return (1);
+}
