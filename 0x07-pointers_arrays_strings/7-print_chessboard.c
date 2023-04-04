1c1cdfda1c57fd0b3b11fdd0e7806882e70ce023
samueladdoenchill <samueladdoenchill@gmail.com>
2023-04-03 22:12:47 -0700

    added files

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,19 +5,15 @@
  * @a: A pointer to the pieces to be printed.
  * Return: void
  **/
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
-	int count, inner_count, row_len = 8;
-
-	for (count = 0; count < row_len; count++)
-	{
-		for (inner_count = 0; inner_count < row_len; inner_count++)
-		{
-			_putchar(a[count][inner_count]);
-			if (inner_count == 7)
-			{
-				_putchar('\n');
-			}
-		}
-	}
+int x, sum1 = 0, sum2 = 0;
+for (x = 0; x < size; x++)
+{
+sum1 += a[x];
+sum2 += a[size - x - 1];
+a += size;
+}
+printf("%d, ", sum1);
+printf("%d\n", sum2);
 }
