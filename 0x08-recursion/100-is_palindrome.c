934a67a12b1ff51bca977d7b06a27a5321c93eff
samueladdoenchill <samueladdoenchill@gmail.com>
2023-04-05 22:44:23 -0700

    100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -15,18 +15,18 @@ return (0);
  * pal_checker - checks for palindrome
  * @s: string
  * @i: iterator
- * @len: length of the string *
+ * @k: length of the string *
  * Return: return 1 if the string is a palindrome,
  * and 0 otherwise.
  */
 
-int pal_checker(char *s, int i, int len)
+int pal_checker(char *s, int i, int k)
 {
-if (s[i] == s[j])
-if (i > j / 2)
+if (s[i] == s[k])
+if (i > k / 2)
 return (1);
 else
-return (pal_checker(s, i + 1, len - 1));
+return (pal_checker(s, i + 1, k - 1));
 else
 return (0);
 }
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * checker - check if two strings are identical.
+ * @s1: string_1 base address.
+ * @s2: string_2 base address.
+ * @i: left index.
+ * @j: special index. (joker)
+ * Return: 1 if s is palindrome, 0 otherwise.
+ */
