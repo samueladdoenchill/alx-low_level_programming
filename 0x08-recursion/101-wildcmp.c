923239972147535102c76a5e0501f454cea52b56
samueladdoenchill <samueladdoenchill@gmail.com>
2023-04-05 22:58:24 -0700

    100

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,20 +1,21 @@
 #include "main.h"
-
 /**
-* wildcmp - compares two strings and returns 1 if both strings identical
-* @s1: string in question
-* @s2: pattern to be used
+* wildcmp - The function compares two strings to see
+* if they are exactly the same.
+* @s1: First string
+* @s2: Second string
 *
-* Return: 1 if identical, 0 otherwise
+* Return: returns 1 if they are exactly
+* the same otherwise 0
 */
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '\0')
-		return (*s1 == '\0');
-	if (*s2 == *s1)
-		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
-	return (0);
+(s2[0] == '\0')
+return (*s1 == '\0');
+if (*s2 == *s1)
+return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+if (*s2 == '*')
+return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+return (0);
 }
