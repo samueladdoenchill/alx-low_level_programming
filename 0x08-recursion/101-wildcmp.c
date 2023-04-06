69af0afd0b148793b401816f483ea59d114f2115
samueladdoenchill <samueladdoenchill@gmail.com>
2023-04-05 23:04:54 -0700

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,9 +9,10 @@
 * the same otherwise 0
 */
 
+
 int wildcmp(char *s1, char *s2)
 {
-(s2[0] == '\0')
+if (*s2 == '\0')
 return (*s1 == '\0');
 if (*s2 == *s1)
 return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
