06a87c2a2551522ec2ff8df18aefd1afb8b89494
samueladdoenchill <samueladdoenchill@gmail.com>
2023-04-10 18:03:50 -0700

    3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - serves as entry point of the program and
+* prints all  arguments passed to the program
+*
+* @argc: The number of arguments passed to the program
+*
+* @argv: The array of strings containing the arguments
+* Return: Always returns 0 to indicate successful execution of the program
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+    char **arg = argv;
+
+    while (*arg != NULL)
+    {
+        printf("%s\n", *arg++);
+    }
+
+    return 0;
+}
+
