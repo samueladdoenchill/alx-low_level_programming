cd2023038d44829a8725b816b7eeb8d3eeb516cc
samueladdoenchill <samueladdoenchill@gmail.com>
2023-04-10 17:41:07 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -13,14 +13,13 @@
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-    if (argc == 1)
-    {
-        printf("%d\n", 0);
-    }
-    else
-    {
-        printf("%d\n", argc - 1);
-    }
-    return 0;
+if (argc == 1)
+{
+printf("%d\n", 0);
 }
-
+else
+{
+printf("%d\n", argc - 1);
+}
+return (0);
+}
