8a26e8d84f67aff5c5802358b548e118d5ee9895
samueladdoenchill <samueladdoenchill@gmail.com>
2023-04-10 18:20:48 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
 * main - serves as entry point of the program and
-* prints all  arguments passed to the program
+* prints the sum of two numbers.
 *
 * @argc: The number of arguments passed to the program
 *
@@ -10,12 +11,18 @@
 * Return: Always returns 0 to indicate successful execution of the program
 */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-char **arg = argv;
-while (*arg != NULL)
+int result;
+if (argc != 3)
 {
-printf("%s\n", *arg++);
+printf("Error\n");
+return (1);
 }
+else
+{
+result = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", result);
 return (0);
+}
 }
