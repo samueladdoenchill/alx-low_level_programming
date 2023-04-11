67e29db1683f0e59fd8a7e41b8c525068f744c25
samueladdoenchill <samueladdoenchill@gmail.com>
2023-04-10 18:58:32 -0700

    5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
-* main - serves as entry point of the program and
-* prints the sum of two numbers.
-*
-* @argc: The number of arguments passed to the program
-*
-* @argv: The array of strings containing the arguments
-* Return: Always returns 0 to indicate successful execution of the program
-*/
+ * * main - serves as entry point of the program and
+ * * prints the sum of two numbers.
+ * *
+ * * @argc: The number of arguments passed to the program
+ * *
+ * * @argv: The array of strings containing the arguments
+ * * Return: Always returns 0 to indicate successful execution of the program
+ */
 
 int main(int argc, char *argv[])
 {
 int sum = 0;
 int i, j;
+char *arg;
 if (argc == 1)
 {
 printf("Error\n");
-return (1)
+return (1);
 }
-for (int i = 1; i < argc; i++)
+for (i = 1; i < argc; i++)
 {
-char *arg = argv[i];
-for (int j = 0; arg[j] != '\0'; j++)
+arg = argv[i];
+for (j = 0; arg[j] != '\0'; j++)
 {
 if (!isdigit(arg[j]))
 {
@@ -31,5 +33,10 @@ printf("Error\n");
 return (1);
 }
 }
-addition += atoi(arg);
+sum += atoi(arg);
+}
+
+printf("%d\n", sum);
+
+return (0);
 }
