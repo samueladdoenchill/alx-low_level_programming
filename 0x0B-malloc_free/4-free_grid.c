ab1428231ab5cd6f17bae811a38364fb3f3d2a84
samueladdoenchill <samueladdoenchill@gmail.com>
2023-04-12 19:34:33 -0700

    added 100

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -12,18 +12,20 @@
 
 void free_grid(int **grid, int height)
 {
-    int i;
+int i;
 
-    if (grid == NULL) {
-        return;
-    }
-
-    for (i = 0; i < height; i++) {
-        if (grid[i] != NULL) {
-            free(grid[i]);
-        }
-    }
-
-    free(grid);
+if (grid == NULL)
+{
+return;
 }
 
+for (i = 0; i < height; i++)
+{
+if (grid[i] != NULL)
+{
+free(grid[i]);
+}
+}
+
+free(grid);
+}
