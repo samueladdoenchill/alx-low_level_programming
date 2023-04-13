f7d32f43df425b823e2344050e70c9878317ebcf
samueladdoenchill <samueladdoenchill@gmail.com>
2023-04-12 18:32:25 -0700

    files

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,18 +12,20 @@
  */
 
 
-char *create_arra
-(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
+    char *array;
+    unsigned int i;
+
     if (size == 0)
         return NULL;
 
-    char *array = malloc(size * sizeof(char));
+    array = malloc(size * sizeof(char));
 
     if (!array)
         return NULL;
 
-    for (unsigned int i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
         array[i] = c;
 
     return array;
