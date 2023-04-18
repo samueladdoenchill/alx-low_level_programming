869cfd4fb903c5b41d07b089b86c3c778e363645
samueladdoenchill <samueladdoenchill@gmail.com>
2023-04-17 17:00:32 -0700

    3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,7 +2,10 @@
 #define ABSOLUTE_VALUE_MACRO
 
 #define ABS(x) \
-    (__extension__ ({ __typeof__(x) _x = (x); _x < 0 ? -_x : _x; }))
+(__extension__ ({ \
+__typeof__(x) _x = (x); \
+_x < 0 ? -_x : _x; \
+}))
 
 #endif
 
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,7 @@
+#ifndef ADDITION_MACRO
+#define ADDITION_MACRO
+
+#define ADD(x, y) ((x) + (y))
+
+#endif
+
