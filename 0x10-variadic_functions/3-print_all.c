a306bcf342af269f583013a7cad04f4a5c1e9c6c
samueladdoenchill <samueladdoenchill@gmail.com>
2023-04-20 23:45:03 -0700

    files

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,37 +7,36 @@
  */
 void print_all(const char * const format, ...)
 {
-    va_list a_list;
-    const char *p;
-    char *str;
-    int first_arg = 1;
+va_list a_list;
+const char *p;
+char *str;
+int first_arg = 1;
 
-    va_start(a_list, format);
+va_start(a_list, format);
 
-    for (p = format; p && *p; p++)
-    {
-        switch (*p)
-        {
-            case 'c':
-                printf("%s%c", first_arg ? "" : ", ", va_arg(a_list, int));
-                break;
-            case 'i':
-                printf("%s%i", first_arg ? "" : ", ", va_arg(a_list, int));
-                break;
-            case 'f':
-                printf("%s%f", first_arg ? "" : ", ", va_arg(a_list, double));
-                break;
-            case 's':
-                str = va_arg(a_list, char *);
-                printf("%s%s", first_arg ? "" : ", ", str ? str : "(nil)");
-                break;
-            default:
-                break;
-        }
-        first_arg = 0;
-    }
-
-    printf("\n");
-    va_end(a_list);
+for (p = format; p && *p; p++)
+{
+switch (*p)
+{
+case 'c':
+printf("%s%c", first_arg ? "" : ", ", va_arg(a_list, int));
+break;
+case 'i':
+printf("%s%i", first_arg ? "" : ", ", va_arg(a_list, int));
+break;
+case 'f':
+printf("%s%f", first_arg ? "" : ", ", va_arg(a_list, double));
+break;
+case 's':
+str = va_arg(a_list, char *);
+printf("%s%s", first_arg ? "" : ", ", str ? str : "(nil)");
+break;
+default:
+break;
+}
+first_arg = 0;
 }
 
+printf("\n");
+va_end(a_list);
+}
