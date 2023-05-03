0d3b273c26cdf369c9add66645d3292f8471ed4b
samueladdoenchill <samueladdoenchill@gmail.com>
2023-05-02 19:29:33 -0700

    added files

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,7 +10,7 @@ int pop_listint(listint_t **head)
 {
 	/* Declare a pointer to the node to be deleted and the value to be returned */
 	listint_t *node_to_delete;
-	int return_value;
+	int value;
 
 	/* Check if the list is empty */
 	if (*head == NULL)
@@ -18,8 +18,9 @@ int pop_listint(listint_t **head)
 		return (0);
 	}
 
+	/* Set the pointer to the node to be deleted and the value to be returned */
 	node_to_delete = *head;
-	value = (node_to_delete->n);
+	value = node_to_delete->n;
 
 	/* Set the head pointer to the next node */
 	*head = (*head)->next;
