4272fc569d0bcb63556551a37acd05b3c6755a4d
samueladdoenchill <samueladdoenchill@gmail.com>
2023-05-12 21:18:38 -0700

    updated 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <elf.h>
 
-/*
- * print_magic - Prints the magic numbers of an ELF header.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
- * Description: Magic numbers are separated by spaces.
-*/
-void print_magic(unsigned char *e_ident) {
+#define EI_NIDENT 16
+
+/**
+ * print_magic - prints the magic number of the ELF header
+ * @e_ident: the ELF header identifier array
+ */
+void print_magic(unsigned char *e_ident)
+{
     int i;
 
-    printf("Magic:   ");
-    for (i = 0; i < EI_NIDENT; i++) {
+    printf("ELF Header:\n  Magic:   ");
+    for (i = 0; i < EI_NIDENT; i++)
         printf("%02x ", e_ident[i]);
-    }
     printf("\n");
 }
 
-/*
- * Function: print_class
- * ---------------------
- * Prints the class of the ELF object file.
- *
- * e_ident: The identification bytes of the ELF header.
+/**
+ * print_class - prints the class of the ELF header
+ * @e_ident: the ELF header identifier array
  */
-void print_class(unsigned char *e_ident) {
-    printf("Class:   ");
-    switch (e_ident[EI_CLASS]) {
+void print_class(unsigned char *e_ident)
+{
+    printf("  Class:                             ");
+    switch (e_ident[EI_CLASS])
+    {
         case ELFCLASSNONE:
             printf("none\n");
             break;
@@ -37,66 +39,46 @@ void print_class(unsigned char *e_ident) {
             break;
         default:
             printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+            break;
     }
 }
 
-/*
- * Function: print_data
- * --------------------
- * Prints the endianness of the ELF object file.
- *
- * e_ident: The identification bytes of the ELF header.
- */
-void print_data(unsigned char *e_ident) {
-    printf("Data:    ");
-    switch (e_ident[EI_DATA]) {
-        case ELFDATANONE:
-            printf("none\n");
-            break;
-        case ELFDATA2LSB:
-            printf("2's complement, little endian\n");
-            break;
-        case ELFDATA2MSB:
-            printf("2's complement, big endian\n");
-            break;
-        default:
-            printf("<unknown: %x>\n", e_ident[EI_DATA]);
-    }
-}
+int main(int argc, char **argv)
+{
+    Elf32_Ehdr *ehdr;
+    int fd;
 
-int main(int argc, char **argv) {
-    FILE *fp;
-    Elf64_Ehdr elf_header;
-
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return 1;
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <ELF file>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    fp = fopen(argv[1], "rb");
-    if (fp == NULL) {
-        perror("fopen");
-        return 1;
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
+    {
+        perror("open");
+        exit(EXIT_FAILURE);
     }
 
-    if (fread(&elf_header, sizeof(elf_header), 1, fp) != 1) {
-        perror("fread");
-        return 1;
+    ehdr = malloc(sizeof(*ehdr));
+    if (ehdr == NULL)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
     }
 
-    if (elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
-            elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
-            elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
-            elf_header.e_ident[EI_MAG3] != ELFMAG3) {
-        fprintf(stderr, "%s is not an ELF file\n", argv[1]);
-        return 1;
+    if (read(fd, ehdr, sizeof(*ehdr)) != sizeof(*ehdr))
+    {
+        fprintf(stderr, "Error: Not a valid ELF file\n");
+        exit(EXIT_FAILURE);
     }
 
-    print_magic(elf_header.e_ident);
-    print_class(elf_header.e_ident);
-    print_data(elf_header.e_ident);
+    print_magic(ehdr->e_ident);
+    print_class(ehdr->e_ident);
 
-    fclose(fp);
-    return 0;
+    free(ehdr);
+    close(fd);
+    return (EXIT_SUCCESS);
 }
 
