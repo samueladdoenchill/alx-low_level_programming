0e63f5d5095b44d093f5f1d842bbec6d34d29be9
samueladdoenchill <samueladdoenchill@gmail.com>
2023-08-21 16:55:21 -0700

    added 1

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* Function to add two integers */
+int add(int a, int b) {
+	return a + b;
+}
+
+/* Function to subtract two integers */
+int sub(int a, int b) {
+	return a - b;
+}
+
+/* Function to multiply two integers */
+int mul(int a, int b) {
+	return a * b;
+}
+
+/* Function to divide two integers */
+int div(int a, int b) {
+	return a / b;
+}
+
+/* Function to find remainder of division between two integers */
+int mod(int a, int b) {
+	return a % b;
+}
