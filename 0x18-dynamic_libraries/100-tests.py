7cde113135ae2d70509659996f83af36a2a01f9c
samueladdoenchill <samueladdoenchill@gmail.com>
2023-08-21 16:59:36 -0700

    added 2

--- 0x18-dynamic_libraries/100-tests.py
+++ 0x18-dynamic_libraries/100-tests.py
@@ -1,9 +1,14 @@
 import random
 import ctypes
 
+# Load the shared library containing the functions
 cops = ctypes.CDLL('./100-operations.so')
+
+# Generate random integers
 a = random.randint(-111, 111)
 b = random.randint(-111, 111)
+
+# Perform arithmetic operations using the shared library
 print("{} + {} = {}".format(a, b, cops.add(a, b)))
 print("{} - {} = {}".format(a, b, cops.sub(a, b)))
 print("{} x {} = {}".format(a, b, cops.mul(a, b)))
